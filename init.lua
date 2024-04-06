7ddf359774a92287fa5fb6c912d3836eaf3e2a44
João Roberto <joao.roberto@ufv.br>
2024-04-05 23:08:19 -0300

    4 tab ident and custom plungins

--- init.lua
+++ init.lua
@@ -105,5 +105,10 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
